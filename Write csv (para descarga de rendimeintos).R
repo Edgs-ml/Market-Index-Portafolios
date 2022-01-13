1104b9feeb4361a504fd40018c9a941548bb16bf
Juanlgj <97475625+Juanlgj@users.noreply.github.com>
2022-01-13 14:09:14 -0600

    Create Write csv (para descarga de rendimeintos).R

---
+++ Write csv (para descarga de rendimeintos).R
@@ -0,0 +1,35 @@
+
+library(readxl)
+  df<-read_excel("C:/Users/juanl/Desktop/paper/Bases de datos/Series de grupos/Charting Excel Export - Jan 2nd 2022 11_56_10 pm.xls")
+
+library(naniar)
+  df<-drop_na(df)
+  
+library(PortfolioAnalytics)
+  
+  colnames(df)<-c("Fecha","OMX","STI","FTSE","HSI","S&P500")
+  
+  FTSE<-xts(df$FTSE,as.Date(df$Fecha))
+  OMX<-xts(df$OMX,as.Date(df$Fecha))
+  STI<-xts(df$STI,as.Date(df$Fecha))
+  HSI<-xts(df$HSI,as.Date(df$Fecha))
+  Sp<-xts(df$`S&P500`,as.Date(df$Fecha))
+  
+  Portafolio<-merge.xts(OMX,STI,FTSE,HSI,Sp)
+  
+  Retornos<-Return.calculate(Portafolio,method = "log")[-1,]
+  Retornos<-data.frame(Retornos)
+  
+  View(Retornos)
+  
+  write.csv(Retornos,"C:\\Users\\juanl\\Desktop\\paper\\Bases de datos\\Series de grupos\\Retornos.csv")
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
