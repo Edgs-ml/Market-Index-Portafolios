8b951e427f686645777c3a73916e0ef9b4ef3001
Juanlgj <97475625+Juanlgj@users.noreply.github.com>
2022-02-16 14:40:32 -0600

    Update Analisis de series (TITULOS).R

--- APLHA/ALPHA 1/ALPHA 1.2/1.2.3. Normality Test/Analisis de series (TITULOS).R
+++ APLHA/ALPHA 1/ALPHA 1.2/1.2.3. Normality Test/Analisis de series (TITULOS).R
@@ -15,80 +15,81 @@ library(statmod)
 library(numDeriv) # Requuerida para cargar la libreria ghyp
 library(ghyp) # multivariado NIG
 library(cramer) # Pruba cramer NIG multivariada
+library(textshape)
 
-g1<- read_excel(insertar direccion GITHUB)
-
+g1<- read.csv("1.2.3.g1 5 menos correlacionados.csv")
+View(g1)
 colnames(g1)<-c("Fecha","SPX","OMXC","FTSE","KOSPI","MSCI")
 
-#View(g1)
-g1<-textshape::column_to_rownames(g1,loc=1)
+View(g1)
+g1<-column_to_rownames(g1,loc=1)
 
 g1<-drop_na(g1)
 glimpse(g1)
-#vis_miss(retornos)
-#View(retornos)
+#vis_miss(g1)
+#View(g1)
 
-retornos<-Return.calculate(g1,method = "log")[-1,]
-#retornos<-drop_na(retornos)
-#View(retornos)
+#g1<-Return.calculate(g1,method = "log")[-1,]
+#g1<-drop_na(g1)
+#View(g1)
 
-Estg1<-basicStats(retornos)
+Estg1<-basicStats(g1)
 Estg1
 
-adf.test(retornos$SPX)
-adf.test(retornos$OMXC)
-adf.test(retornos$FTSE)
-adf.test(retornos$KOSPI)
-adf.test(retornos$MSCI)
+adf.test(g1$SPX)
+adf.test(g1$OMXC)
+adf.test(g1$FTSE)
+adf.test(g1$KOSPI)
+adf.test(g1$MSCI)
 
 #Todas las series son estacionarias
 
 #Shapiro Wilk
-shapiro.test(retornos$SPX)
-shapiro.test(retornos$OMXC)
-shapiro.test(retornos$FTSE)
-shapiro.test(retornos$KOSPI)
-shapiro.test(retornos$MSCI)
+shapiro.test(g1$SPX)
+shapiro.test(g1$OMXC)
+shapiro.test(g1$FTSE)
+shapiro.test(g1$KOSPI)
+shapiro.test(g1$MSCI)
 
 #Shapiro Francia
-sfTest(retornos$SPX)
-sfTest(retornos$OMXC)
-sfTest(retornos$FTSE)
-sfTest(retornos$KOSPI)
-sfTest(retornos$MSCI)
+sfTest(g1$SPX)
+sfTest(g1$OMXC)
+sfTest(g1$FTSE)
+sfTest(g1$KOSPI)
+sfTest(g1$MSCI)
 
 #Construccion de normal con parametros multivariados de la series
-m<-mean(retornos$SPX)
-sd<-sd(retornos$SPX)
-len<-length(retornos$SPX)
+m<-mean(g1$SPX)
+sd<-sd(g1$SPX)
+len<-length(g1$SPX)
 basenormal<-rnorm(len,m,sd)#normal con los parametros de nuestras series
 
 #Prubas de Kormogorov contra distribucion normal, creada con paramaetros de nuestra serie 
-ks.test(retornos$SPX,basenormal)
-ks.test(retornos$OMXC,basenormal)
-ks.test(retornos$FTSE,basenormal)
-ks.test(retornos$KOSPI,basenormal)
-ks.test(retornos$MSCI,basenormal)
+ks.test(g1$SPX,basenormal)
+ks.test(g1$OMXC,basenormal)
+ks.test(g1$FTSE,basenormal)
+ks.test(g1$KOSPI,basenormal)
+ks.test(g1$MSCI,basenormal)
 #Ninguna de las series es normal
 
-mult.norm(retornos)$mult.test
+mult.norm(g1)$mult.test
 #De forma multivariada no hay normalidad
 
 
-plot(density(retornos$SPX),col="blue",ylim=c(0,60), main="Distribuciones contra normal")+
-  lines(density(retornos$OMXC),
+plot(density(g1$SPX),col="blue",ylim=c(0,60), main="Distribuciones contra normal")+
+  lines(density(g1$OMXC),
         col="green")+
-  lines(density(retornos$FTSE),
+  lines(density(g1$FTSE),
         col="orange")+
-  lines(density(retornos$KOSPI),
+  lines(density(g1$KOSPI),
         col="black")+
-  lines(density(retornos$MSCI),
+  lines(density(g1$MSCI),
         col="grey")+
   lines(density(basenormal),
         col="red")
 
 #Parametros de la NIG
-NIG<-nigFit(retornos$OMXC)
+NIG<-nigFit(g1$OMXC)
 
 #Agrupar parametros en un objeto
 a<-NIG@fit[["par"]]
@@ -106,10 +107,10 @@ plot(density(r),
      sub="STI index")
 
 #Pruba de Kormogorov univariada para NIG
-ks.test(retornos$OMXC,r)
+ks.test(g1$OMXC,r)
 
 #Parametros para NIG Multivariada
-multNIG<-fit.NIGmv(data=retornos,silent=FALSE)
+multNIG<-fit.NIGmv(data=g1,silent=FALSE)
 
 #Localizar parametros dentor de un obejto
 Mom1NIGm<-multNIG@expected.value
@@ -117,22 +118,22 @@ Mom2NIGm<-multNIG@variance
 
 #Construccion de la funcion NIG con nuestros  parametros de la funcion multivariada
 Mnig <- rghyp(len,multNIG)
-retornos1<-as.matrix(retornos)
+g11<-as.matrix(g1)
 
 #Prueba cramer de comprobacion
 #Se buscan similitudes estadisticas
-cramer.test(Mnig,retornos1,conf.level = .95)
+cramer.test(Mnig,g11,conf.level = .95)
 
 #Graficas de NIG Multivariada
 plot(density(Mnig),
      col="red",ylim=c(0,60),main="Distribuciones contra NIG multivariada")+
-  lines(density(retornos$SPX),
+  lines(density(g1$SPX),
         col="green")+
-  lines(density(retornos$FTSE),
+  lines(density(g1$FTSE),
         col="purple")+
-  lines(density(retornos$KOSPI),
+  lines(density(g1$KOSPI),
         col="black")+
-  lines(density(retornos$MSCI),
+  lines(density(g1$MSCI),
         col="dark blue")+
-  lines(density(retornos$OMXC),
+  lines(density(g1$OMXC),
         col="pink")
