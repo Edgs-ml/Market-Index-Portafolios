700d23daca4bc11938eb145c4f1da1fb3a85eaac
Juanlgj <juanluisguerra2000@gmail.com>
2022-03-31 10:31:00 -0600

    Optimizacion de Alpha 1.2

--- APLHA/ALPHA 1/ALPHA 1.2/1.2.4 Optimización/1.2.4.Optimización_NIG_ALPHA1.2.R
+++ APLHA/ALPHA 1/ALPHA 1.2/1.2.4 Optimización/1.2.4.Optimización_NIG_ALPHA1.2.R
@@ -1,3 +1,4 @@
+
 library(quantmod)
 library(PerformanceAnalytics)
 library(PortfolioAnalytics)
@@ -6,34 +7,36 @@ library(readxl)
 library(fBasics)
 library(ghyp)
 
+
 g1<- read_excel("D:/JL/Market-Index-Portafolios/APLHA/ALPHA 1/ALPHA 1.2/1.2.4 Optimización/Optimizacion NIG alpha 1.2.xlsx", 
                 sheet = "Datos")
+
 View(g1)
 #Esta base de datos ya tiene los retornos de los indices, no sacar retornos otra vez
 
-colnames(g1)<-c("Fecha","DJI",	"HSI",	"OMX20",	"STI",	"FTSE")
+colnames(g1)<-c("Fecha","SPX",	"OMXC",	"FTSE","KOSPI","MSCI")
 
-DJI<-xts(g1$DJI,as.Date(g1$Fecha))
-HSI<-xts(g1$HSI,as.Date(g1$Fecha))
-OMX20<-xts(g1$OMX20,as.Date(g1$Fecha))
-STI<-xts(g1$STI,as.Date(g1$Fecha))
+SPX<-xts(g1$SPX,as.Date(g1$Fecha))
+OMXC<-xts(g1$OMXC,as.Date(g1$Fecha))
 FTSE<-xts(g1$FTSE,as.Date(g1$Fecha))
+KOSPI<-xts(g1$KOSPI,as.Date(g1$Fecha))
+MSCI<-xts(g1$MSCI,as.Date(g1$Fecha))
 
-Portafolio<-merge.xts(DJI,HSI,OMX20,STI,FTSE)
+Portafolio_NIG_A1.2<-merge.xts(SPX,OMXC,FTSE,KOSPI,MSCI)
 
 #Los datos ya son los retornos, no es necesario volverlos a sacar. 
 #Returns<- Return.calculate(PreciosAd, method = "log")[-1]
 
-Specs_Port1 <- portfolio.spec(c("DJI",	"HSI",	"OMX20",	"STI",	"FTSE"))
+Specs_Port_NIG_A1.2 <- portfolio.spec(c("SPX",	"OMXC",	"FTSE","KOSPI","MSCI"))
 
 ##### Add Constraints #####
-Specs_Port1 <- add.constraint(Specs_Port1,type="full_investment")
-Specs_Port1 <- add.constraint(Specs_Port1,type="long_only")
+Specs_Port_NIG_A1.2 <- add.constraint(Specs_Port_NIG_A1.2,type="full_investment")
+Specs_Port_NIG_A1.2 <- add.constraint(Specs_Port_NIG_A1.2,type="long_only")
 
 ##### Add Objective #####
-Specs_Port1 <- add.objective(Specs_Port1,type="risk",name="StdDev")
-Specs_Port1 <- add.objective(Specs_Port1,type='return',name='mean')
-Specs_Port1
+Specs_Port_NIG_A1.2 <- add.objective(Specs_Port_NIG_A1.2,type="risk",name="StdDev")
+Specs_Port_NIG_A1.2 <- add.objective(Specs_Port_NIG_A1.2,type='return',name='mean')
+Specs_Port_NIG_A1.2
 
 covnig<-function(R,portfolio){
   a<-fit.NIGmv(R,silent=TRUE)
@@ -44,25 +47,28 @@ covnig<-function(R,portfolio){
   return(resultado)
 }
 
-Optimized_Port1 <- optimize.portfolio(Portafolio,
-                                      Specs_Port1,
+Optimized_Port_NIG_A1.2 <- optimize.portfolio(Portafolio_NIG_A1.2,
+                                      Specs_Port_NIG_A1.2,
                                       momentFUN = covnig,
                                       optimize_method = "random",
                                       trace = TRUE)
 
-chart.Weights(Optimized_Port1,plot.type = "barplot")
-W_R <- extractWeights(Optimized_Port1)
+chart.Weights(Optimized_Port_NIG_A1.2,plot.type = "barplot")
+W_R <- extractWeights(Optimized_Port_NIG_A1.2)
 sum(W_R)
 
-Return_opt1 <- Return.portfolio(Portafolio,W_R)
+Return_opt_NIG_A1.2 <- Return.portfolio(Portafolio_NIG_A1.2,W_R)
 
-table.AnnualizedReturns(Return_opt1,
+table.AnnualizedReturns(Return_opt_NIG_A1.2,
                         scale = 252,
                         geometric = FALSE)
-Return.cumulative(Return_opt1,
+Return.cumulative(Return_opt_NIG_A1.2,
                   geometric = FALSE)
 
-chart.RiskReward(Optimized_Port1,
+chart.RiskReward(Optimized_Port_NIG_A1.2,
                  risk.col = 'StdDev',
                  return.col = 'mean',
                  chart.assets = T)
+
+
+
---
+++ APLHA/ALPHA 1/ALPHA 1.2/1.2.4 Optimización/1.2.4.Optimización_Normal_ALPHA1.2..R
@@ -0,0 +1,65 @@
+# 1.2.4. Optimizaci√≥n de Portafolio ALPHA 1.2.
+library(quantmod)
+library(PerformanceAnalytics)
+library(PortfolioAnalytics)
+library(DEoptim)
+library(readxl)
+library(fBasics)
+library(ghyp)
+
+g1<- read_excel("D:/JL/Market-Index-Portafolios/APLHA/ALPHA 1/ALPHA 1.2/1.2.4 OptimizaciÛn/Optimizacion NIG alpha 1.2.xlsx", 
+                sheet = "Datos")
+
+#Esta base de datos ya tiene los retornos de los indices, no sacar retornos otra vez
+
+colnames(g1)<-c("Fecha","SPX",	"OMXC",	"FTSE","KOSPI","MSCI")
+
+
+SPX<-xts(g1$SPX,as.Date(g1$Fecha))
+OMXC<-xts(g1$OMXC,as.Date(g1$Fecha))
+FTSE<-xts(g1$FTSE,as.Date(g1$Fecha))
+KOSPI<-xts(g1$KOSPI,as.Date(g1$Fecha))
+MSCI<-xts(g1$MSCI,as.Date(g1$Fecha))
+
+Portafolio_NORM_A1.2<-merge.xts(SPX,OMXC,FTSE,KOSPI,MSCI)
+
+#Los datos ya son los retornos, no es necesario volverlos a sacar. 
+#Returns<- Return.calculate(PreciosAd, method = "log")[-1]
+
+Specs_Port_NORM_A1.2 <- portfolio.spec(c("SPX",	"OMXC",	"FTSE",	"KOSPI",	"MSCI"))
+
+##### Add Constraints #####
+Specs_Port_NORM_A1.2 <- add.constraint(Specs_Port_NORM_A1.2,type="full_investment")
+Specs_Port_NORM_A1.2 <- add.constraint(Specs_Port_NORM_A1.2,type="long_only")
+
+##### Add Objective #####
+Specs_Port_NORM_A1.2 <- add.objective(Specs_Port_NORM_A1.2,type="risk",name="StdDev")
+Specs_Port_NORM_A1.2 <- add.objective(Specs_Port_NORM_A1.2,type='return',name='mean')
+Specs_Port_NORM_A1.2
+
+Optimized_Port_NORM_A1.2 <- optimize.portfolio(Portafolio_NORM_A1.2,
+                                          Specs_Port_NORM_A1.2,
+                                          optimize_method = "random",
+                                          trace = TRUE)
+
+chart.Weights(Optimized_Port_NORM_A1.2,plot.type="barplot")
+W_R_NORM_A1.2 <- extractWeights(Optimized_Port_NORM_A1.2)
+sum(W_R_NORM_A1.2)
+
+Return_opt_NORM_A1.2 <- Return.portfolio(Portafolio_NORM_A1.2,
+                                    W_R_NORM_A1.2)
+
+table.AnnualizedReturns(Return_opt_NORM_A1.2,
+                        scale = 252,
+                        geometric = FALSE)
+
+Return.cumulative(Return_opt_NORM_A1.2,
+                  geometric = FALSE)
+
+chart.RiskReward(Optimized_Port_NORM_A1.2,
+                 risk.col = 'StdDev',
+                 return.col = 'mean',
+                 chart.assets = T)
+
+
+
