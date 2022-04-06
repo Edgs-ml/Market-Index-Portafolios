2615d7d3f2556fbd62cfec323ee3053868b7a477
Edgar Soto <68365509+Edgs-ml@users.noreply.github.com>
2022-04-05 20:02:01 -0500

    completo

--- APLHA/ALPHA 1/ALPHA 1.1/1.1.3Portafolio_Optimization/Optimziacion en R/Optimizacion en R NIG.R
+++ APLHA/ALPHA 1/ALPHA 1.1/1.1.3Portafolio_Optimization/Optimziacion en R/Optimizacion en R NIG.R
@@ -26,16 +26,22 @@ rendimientos <- xts(g1[,2:6], order.by = as.Date(g1$Fecha))
 #Los datos ya son los retornos, no es necesario volverlos a sacar. 
 #Returns<- Return.calculate(PreciosAd, method = "log")[-1]
 
-Specs_Port1 <- portfolio.spec(c("DJI",	"HSI",	"OMX20",	"STI",	"FTSE"))
+#------------- Creación de portafolio
+Specs_Port <- portfolio.spec(c("DJI",	"HSI",	"OMX20",	"STI",	"FTSE"))
 
 ##### Add Constraints #####
-Specs_Port1 <- add.constraint(Specs_Port1,type="full_investment")
-Specs_Port1 <- add.constraint(Specs_Port1,type="long_only")
+Specs_Port <- add.constraint(Specs_Port,type="full_investment")
+Specs_Port <- add.constraint(Specs_Port,type="long_only")
 
 ##### Add Objective #####
-Specs_Port1 <- add.objective(Specs_Port1,type="risk",name="StdDev")
-Specs_Port1 <- add.objective(Specs_Port1,type='return',name='mean')
-Specs_Port1
+Specs_Port <- add.objective(Specs_Port,type="risk",name="StdDev")
+Specs_Port <- add.objective(Specs_Port,type='return',name='mean')
+Specs_Port
+
+Optimized_Port_Normal <- optimize.portfolio(rendimientos,
+                                      Specs_Port,
+                                      optimize_method = "random",
+                                      trace = TRUE)
 
 covnig<-function(R,portfolio){
   a<-fit.NIGmv(R,silent=TRUE)
@@ -46,72 +52,84 @@ covnig<-function(R,portfolio){
   return(resultado)
 }
 
-Optimized_Port1 <- optimize.portfolio(rendimientos,
-                                      Specs_Port1,
-                                      momentFUN = covnig,
-                                      optimize_method = "random",
-                                      trace = TRUE)
+Optimized_Port_NIG <- optimize.portfolio(rendimientos,
+                                         Specs_Port,
+                                         momentFUN = covnig,
+                                         optimize_method = "random",
+                                         trace = TRUE)
+#--------------
+#-------------- Análisis de portafolios
+chart.Weights(Optimized_Port_Normal, plot.type = "barplot")
+W_R_Normal <- extractWeights(Optimized_Port_Normal)
+W_R_Normal
+sum(W_R_Normal)
 
-chart.Weights(Optimized_Port1, plot.type = "barplot")
-W_R <- extractWeights(Optimized_Port1)
-W_R
-sum(W_R)
+chart.Weights(Optimized_Port_NIG, plot.type = "barplot")
+W_R_NIG <- extractWeights(Optimized_Port_NIG)
+W_R_NIG
+sum(W_R_NIG)
 
-Return_opt1 <- Return.portfolio(rendimientos, W_R)
+Return_Port_Normal <- Return.portfolio(rendimientos, W_R_Normal)
 
-table.AnnualizedReturns(Return_opt1,
+table.AnnualizedReturns(Return_Port_Normal,
                         scale = 252,
                         geometric = FALSE)
 
-Return.cumulative(Return_opt1,
+Return.cumulative(Return_Port_Normal,
                   geometric = FALSE)
 
-chart.RiskReward(Optimized_Port1,
+chart.RiskReward(Optimized_Port_Normal,
                  risk.col = 'StdDev',
                  return.col = 'mean',
                  chart.assets = TRUE)
 
+Return_Port_NIG <- Return.portfolio(rendimientos, W_R_NIG)
+
+table.AnnualizedReturns(Return_Port_NIG,
+                        scale = 252,
+                        geometric = FALSE)
+
+Return.cumulative(Return_Port_NIG,
+                  geometric = FALSE)
+
+chart.RiskReward(Optimized_Port_NIG,
+                 risk.col = 'StdDev',
+                 return.col = 'mean',
+                 chart.assets = TRUE)
+#--------------Graficas comparativas
 rand <- Optimized_Port1$random_portfolios
 # 1794 portafolios de diferentes pesos al asar 
 
 stdv <- Optimized_Port1$random_portfolio_objective_results[[1]]$objective_measures$StdDev
 
-
 medias <- Optimized_Port1$random_portfolio_objective_results[[1]]$objective_measures$mean
 
 
 #--------
+mediasNORM <- NULL
+standDevNORM <- NULL
+for (i in 1:1682) {
+  mediasNORM[i] <- Optimized_Port_Normal$random_portfolio_objective_results[[i]]$objective_measures$mean
+  standDevNORM[i] <- Optimized_Port_Normal$random_portfolio_objective_results[[i]]$objective_measures$StdDev
+}
 
 mediasNIG <- NULL
 standDeviNIG <- NULL
-for (i in 1:1794) {
-  medias[i] <- Optimized_Port1$random_portfolio_objective_results[[i]]$objective_measures$mean
-  standDevi[i] <- Optimized_Port1$random_portfolio_objective_results[[i]]$objective_measures$StdDev
+for (i in 1:1768) {
+  mediasNIG[i] <- Optimized_Port_NIG$random_portfolio_objective_results[[i]]$objective_measures$mean
+  standDeviNIG[i] <- Optimized_Port_NIG$random_portfolio_objective_results[[i]]$objective_measures$StdDev
 }
-
-Optimized_Port2 <- optimize.portfolio(rendimientos,
-                                      Specs_Port1,
-                                      optimize_method = "random",
-                                      trace = TRUE)
-mediasNORM <- NULL
-standDevNORM <- NULL
-for (i in 1:1722) {
-  mediasNORM[i] <- Optimized_Port2$random_portfolio_objective_results[[i]]$objective_measures$mean
-  standDevNORM[i] <- Optimized_Port2$random_portfolio_objective_results[[i]]$objective_measures$StdDev
-}
-
 #-------- Fronteras
 fronteraNorm <- tibble(Volatility = standDevNORM, Expected_Return = mediasNORM)
-fronteraNIG <- tibble(Volatility = standDevi, Expected_Return = medias)
-
+fronteraNIG <- tibble(Volatility = standDeviNIG, Expected_Return = mediasNIG)
 
 fronteraNorm %>%
-  ggplot(aes(x=volatilidad, y=ExpectedRet))+ #portafolios al asar de la normal Chicago55
+  ggplot(aes(x=Volatility, y=Expected_Return))+
   geom_point(alpha=0.5, col="pink")+
-  geom_point(data=tibble(volatilidad = Optimized_Port2$objective_measures$StdDev,
-                         ExpectedRet = Optimized_Port2$objective_measures$mean),
-             col="red", size=3)+ #optimo de la normal Chicago20
-  geom_point(data=fronteraNIG, aes(x=volatilidad, y=ExpectedRet), alpha=0.2, col="blue")+ #portafolios al asar de la NIG Toki055
-  geom_point(data=tibble(volatilidad = Optimized_Port1$objective_measures$StdDev, 
-                         ExpectedRet = Optimized_Port1$objective_measures$mean), 
-             alpha=1, col="darkgreen", size=3) #Tokio45
+  geom_point(data=tibble(Volatility = Optimized_Port_Normal$objective_measures$StdDev,
+                         Expected_Return = Optimized_Port_Normal$objective_measures$mean),
+             col="red", size=3)+
+  geom_point(data=fronteraNIG, aes(x=Volatility, y=Expected_Return), alpha=0.2, col="blue")+
+  geom_point(data=tibble(Volatility = Optimized_Port_NIG$objective_measures$StdDev, 
+                         Expected_Return = Optimized_Port_NIG$objective_measures$mean), 
+             alpha=1, col="darkgreen", size=3)
