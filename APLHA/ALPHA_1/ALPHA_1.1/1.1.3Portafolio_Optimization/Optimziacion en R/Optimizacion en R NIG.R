58b61c7860009560370f97662e3d22302dfd86be
Edgar Soto <68365509+Edgs-ml@users.noreply.github.com>
2022-04-27 19:29:00 -0500

    d

--- APLHA/ALPHA_1/ALPHA_1.1/1.1.3Portafolio_Optimization/Optimziacion en R/Optimizacion en R NIG.R
+++ APLHA/ALPHA_1/ALPHA_1.1/1.1.3Portafolio_Optimization/Optimziacion en R/Optimizacion en R NIG.R
@@ -8,12 +8,15 @@ library(ghyp)
 library(naniar)
 library(tidyverse)
 
-g1<- read_excel("D:/JL/Market-Index-Portafolios/APLHA/ALPHA 1/ALPHA 1.2/1.2.4 Optimizaci?n/Optimizacion NIG alpha 1.2.xlsx", 
-                sheet = "Datos")
+#g1<- read_excel("D:/JL/Market-Index-Portafolios/APLHA/ALPHA 1/ALPHA 1.2/1.2.4 Optimizaci?n/Optimizacion NIG alpha 1.2.xlsx", 
+#                sheet = "Datos")
+
+g1 <- Optimizacion_NIG_alpha_1_2
 View(g1)
 #Esta base de datos ya tiene los retornos de los indices, no sacar retornos otra vez
 
 colnames(g1)<-c("Fecha","DJI","HSI","OMX20","STI","FTSE")
+g1 <- g1[,-7]
 
 g1 %>%
   vis_miss()
@@ -22,6 +25,7 @@ g1 <- g1 %>%
   drop_na()
 
 rendimientos <- xts(g1[,2:6], order.by = as.Date(g1$Fecha))
+View(rendimientos)
 
 #Los datos ya son los retornos, no es necesario volverlos a sacar. 
 #Returns<- Return.calculate(PreciosAd, method = "log")[-1]
@@ -59,6 +63,7 @@ Optimized_Port_NIG <- optimize.portfolio(rendimientos,
                                          trace = TRUE)
 #--------------
 #-------------- Análisis de portafolios
+
 chart.Weights(Optimized_Port_Normal, plot.type = "barplot")
 W_R_Normal <- extractWeights(Optimized_Port_Normal)
 W_R_Normal
@@ -96,7 +101,9 @@ chart.RiskReward(Optimized_Port_NIG,
                  risk.col = 'StdDev',
                  return.col = 'mean',
                  chart.assets = TRUE)
+
 #--------------Graficas comparativas
+
 rand <- Optimized_Port1$random_portfolios
 # 1794 portafolios de diferentes pesos al asar 
 
@@ -120,6 +127,7 @@ for (i in 1:1768) {
   standDeviNIG[i] <- Optimized_Port_NIG$random_portfolio_objective_results[[i]]$objective_measures$StdDev
 }
 #-------- Fronteras
+
 fronteraNorm <- tibble(Volatility = standDevNORM, Expected_Return = mediasNORM)
 fronteraNIG <- tibble(Volatility = standDeviNIG, Expected_Return = mediasNIG)
 
